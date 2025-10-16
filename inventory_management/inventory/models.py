5ac79c00642926d4402b7353d1af8e1c37714884
drahmetacik <a.acik@icloud.com>
2025-10-16 22:03:14 +0100

    add validation to Product model's quantity field to ensure it is not negative

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(validators=[MinValueValidator(0)])
 
     def __str__(self):
         return self.name
